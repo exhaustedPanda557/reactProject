df78c03aaf7eba6494d7eaf7f26c3e1b41e791b0
Amanda-Levy <amanda_levy@brown.edu>
2022-11-17 10:07:50 -0500

    formating boxes

--- src/DinnerItem.js
+++ src/DinnerItem.js
@@ -1,13 +1,24 @@
 
 export default function DinnerItem({ data, setDinner, dinner }) {
-    const { name, calories, status, category, image } = data;
+    const { name, calories, status, category, prepTime, image } = data;
 
     const handleClick = event => {
         const foodName = event.target.value;
         setDinner([...dinner, foodName]);
     }
 
-    return (<div className="foodBox"><img src={image} className="boxImage"/><button onClick={handleClick} value={name}>Add To Dinner Recipe
-    
-    </button></div>)
+    return (
+    <div className="foodBox">
+        <div>
+        <img src={image} className="boxImage" />
+        <h3>{name}</h3>
+        </div>
+        <div>
+        <p>Calories: {calories}</p>
+        <p>Status: {status}</p>
+        <p>Food Category: {category}</p>
+        <p>Prep Time: {prepTime}</p>
+        </div>
+        <button onClick={handleClick} value={name}>Add To Dinner Recipe</button>
+    </div>)
 };
